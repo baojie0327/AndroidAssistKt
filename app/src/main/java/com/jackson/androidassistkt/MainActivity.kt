f98e87643d258fede1c9911cfae0120537d5c927
Jackson <bj0327_work@163.com>
2020-11-13 13:43:32 +0800

    init

---
+++ app/src/main/java/com/jackson/androidassistkt/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.jackson.androidassistkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jackson.androidassistkt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+    }
+}
